031a0fcd40a25ad387fc168e773dcb0a7e227fa6
soonho <shyoon991@gmail.com>
2023-07-31 15:09:12 +0900

    웹 프로젝트 환경 구성

--- part1/mvc-practice/src/main/java/org/example/WebApplicationServer.java
+++ part1/mvc-practice/src/main/java/org/example/WebApplicationServer.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
 public class WebApplicationServer {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "mvc-practice/webapps/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
