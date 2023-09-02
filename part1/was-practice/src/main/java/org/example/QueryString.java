2465f704b93f7d48758adb19e0d9f2941e5986f9
soonho <shyoon991@gmail.com>
2023-09-03 08:04:15 +0900

    QueryString 객체 생성 테스트

---
+++ part1/was-practice/src/main/java/org/example/QueryString.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class QueryString {
+    private final String key;
+    private final String value;
+
+    public QueryString(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
