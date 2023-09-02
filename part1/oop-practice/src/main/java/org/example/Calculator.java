31d0452d332f36d2564eefce5110617de03b96d5
soonho <shyoon991@gmail.com>
2023-09-02 15:21:16 +0900

    덧셈 연산 테스트 -  개선 (Refactor) : 테스트는 계속 성공함을 유지하면서 코드를 개선

--- part1/oop-practice/src/main/java/org/example/Calculator.java
+++ part1/oop-practice/src/main/java/org/example/Calculator.java
@@ -1,8 +1,10 @@
 package org.example;
 
 public class Calculator {
-    public static int calculate(int operand, String operator, int operand2) {
-
-        return 3; // 코드의 완성도보다는 테스트의 성공을 목적으로 작성합니다.
+    public static int calculate(int operand1, String operator, int operand2) {
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
+        }
+        return 0;
     }
 }
