3d000a07dcdaad9b7ea6488188f1ae51082a6f72
soonho <shyoon991@gmail.com>
2023-09-02 17:03:14 +0900

    음식점 - 요리 객체 생성 테스트

---
+++ part1/oop-practice/src/main/java/org/example/customer/Cook.java
@@ -0,0 +1,12 @@
+package org.example.customer;
+
+public class Cook {
+    private String name;
+    private int price;
+
+    public Cook(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
---
+++ part1/oop-practice/src/test/java/org/example/customer/CookTest.java
@@ -0,0 +1,17 @@
+package org.example.customer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CookTest {
+
+    @DisplayName("요리를 생성한다.")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new Cook("만두", 5_000))
+                .doesNotThrowAnyException();
+    }
+
+}
