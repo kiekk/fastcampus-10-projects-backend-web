dfd335751091e41685c10cc09b3792e09eed6f99
soonho <shyoon991@gmail.com>
2023-09-02 16:22:01 +0900

    학점 계산기 테스트 - 실패 (Red)

---
+++ part1/oop-practice/src/main/java/org/example/grade/GradeCalculator.java
@@ -0,0 +1,17 @@
+package org.example.grade;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double calculateGrade() {
+        return 0;
+    }
+
+}
