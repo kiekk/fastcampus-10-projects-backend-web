174633d092a3bbc5b244787abb77b7ef70896484
soonho <shyoon991@gmail.com>
2023-09-03 08:04:03 +0900

    RequestLine 객체 생성 테스트

---
+++ part1/was-practice/src/main/java/org/example/RequestLine.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.Objects;
+
+public class RequestLine {
+
+
+    private final String method;
+    private final String urlPath;
+    private String queryString;
+
+    public RequestLine(String requestLine) {
+        String[] tokens = requestLine.split(" ");
+        this.method = tokens[0];
+
+        String[] urlPathTokens = tokens[1].split("\\?");
+        this.urlPath = urlPathTokens[0];
+
+        if (urlPathTokens.length == 2) {
+            this.queryString = urlPathTokens[1];
+        }
+    }
+
+    public RequestLine(String method, String urlPath, String queryString) {
+        this.method = method;
+        this.urlPath = urlPath;
+        this.queryString = queryString;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUrlPath() {
+        return urlPath;
+    }
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RequestLine that = (RequestLine) o;
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryString, that.queryString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, urlPath, queryString);
+    }
+
+}
