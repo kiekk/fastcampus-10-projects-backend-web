388ee2a6129c5792d2535a9b4da268e664ad217f
soonho <shyoon991@gmail.com>
2023-09-03 07:30:52 +0900

    Socket, ServerSocket을 사용하여 웹 요청 받기

--- part1/was-practice/src/main/java/org/example/Main.java
+++ part1/was-practice/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.io.IOException;
+
+// GET /calculate?operand1=11&operator=+&operand2=5 --> 계산 결과 리턴
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
     }
 }
