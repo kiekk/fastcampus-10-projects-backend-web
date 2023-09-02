9352a8960fa34c8397fa3604ac2a8c4931ad4d1c
soonho <shyoon991@gmail.com>
2023-09-02 16:29:51 +0900

    학점 계산기 테스트 - 개선 (Refactor)

--- part1/oop-practice/src/main/java/org/example/grade/GradeCalculator.java
+++ part1/oop-practice/src/main/java/org/example/grade/GradeCalculator.java
@@ -4,14 +4,27 @@ import java.util.List;
 
 public class GradeCalculator {
 
-    private List<Course> courses;
+    private final List<Course> courses;
 
     public GradeCalculator(List<Course> courses) {
         this.courses = courses;
     }
 
+    /**
+     * 요구사항
+     * 평균학점 계산 방법 - (학점수*교과목 평점)의 합계 / 수강신청 총학점 수
+     * 일급 컬렉션 사용
+     */
     public double calculateGrade() {
-        return 4.5;
+        // (학점수*교과목 평점)의 합계
+        double multipliedCreditAndCourseGrade = 0;
+        for (Course course : courses) {
+            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+        }
+
+        // 수강신청 총학점 수
+        int totalCompletedCredit = courses.stream().mapToInt(Course::getCredit).sum();
+        return multipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 
 }
