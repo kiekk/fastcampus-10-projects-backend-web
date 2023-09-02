0d4301d600c086619d0b9e64dde04a4d0c85057d
soonho <shyoon991@gmail.com>
2023-09-02 15:19:51 +0900

    덧셈 연산 테스트 - 성공 (Green) : 테스트를 빠르게 성공하는 것이 목적

--- part1/oop-practice/src/main/java/org/example/Calculator.java
+++ part1/oop-practice/src/main/java/org/example/Calculator.java
@@ -3,6 +3,6 @@ package org.example;
 public class Calculator {
     public static int calculate(int operand, String operator, int operand2) {
 
-        return 0;
+        return 3; // 코드의 완성도보다는 테스트의 성공을 목적으로 작성합니다.
     }
 }
