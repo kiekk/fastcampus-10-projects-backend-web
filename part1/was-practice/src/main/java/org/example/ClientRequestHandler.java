94592e45a32e9aad4f24d296cd375f4f918fe004
soonho <shyoon991@gmail.com>
2023-09-03 08:51:08 +0900

    Thread Pool을 사용하여 사용자 요청을 처리

--- part1/was-practice/src/main/java/org/example/ClientRequestHandler.java
+++ part1/was-practice/src/main/java/org/example/ClientRequestHandler.java
@@ -20,10 +20,6 @@ public class ClientRequestHandler implements Runnable {
 
     @Override
     public void run() {
-        /**
-         * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
-         */
-
         logger.info("[ClientRequestHandler] new client {} started.", Thread.currentThread().getName());
         try (InputStream in = clientSocket.getInputStream();
              OutputStream out = clientSocket.getOutputStream()) {
--- part1/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ part1/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -6,10 +6,13 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -26,7 +29,10 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected.");
 
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                /**
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                 */
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
