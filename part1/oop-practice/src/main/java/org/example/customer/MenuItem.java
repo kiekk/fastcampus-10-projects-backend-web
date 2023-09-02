d895025b7c1237ec7a7b15e09f3a4725de6f508a
soonho <shyoon991@gmail.com>
2023-09-02 17:05:20 +0900

    음식점 - 메뉴 항목 객체 생성 테스트

---
+++ part1/oop-practice/src/main/java/org/example/customer/MenuItem.java
@@ -0,0 +1,12 @@
+package org.example.customer;
+
+public class MenuItem {
+    private String name;
+    private int price;
+
+    public MenuItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
