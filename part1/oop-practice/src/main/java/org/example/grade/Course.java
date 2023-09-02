b3aa24b3d7aec694d4e3ff8789813d8b752f751b
soonho <shyoon991@gmail.com>
2023-09-02 16:21:51 +0900

    학점 계산기 - Course 객체 생성 테스트

---
+++ part1/oop-practice/src/main/java/org/example/grade/Course.java
@@ -0,0 +1,14 @@
+package org.example.grade;
+
+public class Course {
+
+    private final String subject;
+    private final int credit;
+    private final String grade;
+
+    public Course(String subject, int credit, String grade) {
+        this.subject = subject;
+        this.credit = credit;
+        this.grade = grade;
+    }
+}
---
+++ part1/oop-practice/src/test/java/org/example/grade/CourseTest.java
@@ -0,0 +1,17 @@
+package org.example.grade;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CourseTest {
+
+    @DisplayName("과목(코스)를 생성한다.")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new Course("OOP", 3, "A+"))
+                .doesNotThrowAnyException();
+    }
+
+}
