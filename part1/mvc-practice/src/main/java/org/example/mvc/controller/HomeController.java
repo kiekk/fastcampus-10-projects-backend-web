efc2c2fca3f98cc18cb84e59ccced18705a177ad
soonho <shyoon991@gmail.com>
2023-09-03 16:23:58 +0900

    remove annotation Controller

--- part1/mvc-practice/src/main/java/org/example/mvc/controller/HomeController.java
+++ part1/mvc-practice/src/main/java/org/example/mvc/controller/HomeController.java
@@ -1,15 +1,13 @@
 package org.example.mvc.controller;
 
-import org.example.mvc.annotation.Controller;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
-public class HomeController implements org.example.mvc.controller.Controller {
+public class HomeController implements Controller {
 
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
         return "home.jsp";
     }
+
 }
