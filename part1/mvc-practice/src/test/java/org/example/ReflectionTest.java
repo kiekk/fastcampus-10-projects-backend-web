7ed4f5eecc0127c00773875002ae9c92ba9f3e3c
soonho <shyoon991@gmail.com>
2023-09-03 12:04:10 +0900

    Reflection Test - 힙 영역에 로드되어 있는 클래스 타입의 객체 가져오기

--- part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
 import org.example.model.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -13,6 +14,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @Controller 애노테이션이 설정되어 있는 모든 클래스를 찾아서 출력한다.
@@ -36,6 +39,27 @@ public class ReflectionTest {
         logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).toList());
         logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).toList());
         logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).toList());
+    }
+
+    @DisplayName("힙 영역에 로드되어 있는 클래스 타입의 객체를 가져온다.")
+    @Test
+    void load() throws ClassNotFoundException {
+        // 1
+        Class<User> clazz1 = User.class;
+
+        // 2
+        User user = new User("test", "test");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz1 : [{}]", clazz1);
+        logger.debug("clazz2 : [{}]", clazz2);
+        logger.debug("clazz3 : [{}]", clazz3);
+
+        assertThat(clazz1 == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
     }
 
     private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
