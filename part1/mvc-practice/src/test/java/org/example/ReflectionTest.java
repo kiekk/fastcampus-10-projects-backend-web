0985c7c01096afd02c24b129f9704317236b1622
soonho <shyoon991@gmail.com>
2023-09-03 11:56:32 +0900

    Reflection Test 리팩토링

--- part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -1,13 +1,15 @@
 package org.example;
 
 import org.example.annotation.Controller;
+import org.example.annotation.Service;
 import org.junit.jupiter.api.Test;
-
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -19,11 +21,17 @@ public class ReflectionTest {
 
     @Test
     void componentScan() {
-        Reflections reflections = new Reflections("org.example");
-
-        Set<Class<?>> beans = new HashSet<>(reflections.getTypesAnnotatedWith(Controller.class));
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         logger.debug("beans : [{}]", beans);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> beans = new HashSet<>();
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+        return beans;
     }
 
 }
