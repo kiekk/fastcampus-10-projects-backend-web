13e49ba38fdbd33136717f8cc5e55150afe37038
soonho <shyoon991@gmail.com>
2023-09-03 11:21:57 +0900

    유저 조회 테스트 - 개선 (Refactor) - 유저 조회 쿼리 리팩토링

--- part1/jdbc-practice/src/main/java/org/example/JdbcTemplate.java
+++ part1/jdbc-practice/src/main/java/org/example/JdbcTemplate.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTemplate {
@@ -32,4 +33,45 @@ public class JdbcTemplate {
         }
     }
 
+    public Object executeQuery(String sql, PreparedStatementSetter pss, RowMapper rowMapper) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pss.setter(pstmt);
+
+            rs = pstmt.executeQuery();
+
+            Object obj = null;
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
+            }
+
+            return obj;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (Exception e) {
+
+                }
+            }
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (Exception e) {
+
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
 }
--- part1/jdbc-practice/src/main/java/org/example/UserDao.java
+++ part1/jdbc-practice/src/main/java/org/example/UserDao.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
@@ -17,49 +14,14 @@ public class UserDao {
     }
 
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM USERS WHERE userId = ?";
-            pstmt = con.prepareStatement(sql);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return (User) jdbcTemplate.executeQuery("SELECT * FROM USERS WHERE userId = ?", pstmt -> {
             pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email")
-                );
-            }
-            return user;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (Exception e) {
-
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (Exception e) {
-
-                }
-            }
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (Exception e) {
-                }
-            }
-        }
+        }, rs -> new User(
+                rs.getString("userId"),
+                rs.getString("password"),
+                rs.getString("name"),
+                rs.getString("email")
+        ));
     }
 }
