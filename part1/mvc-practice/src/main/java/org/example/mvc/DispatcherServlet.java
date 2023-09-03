c202d574e24383eeb28d8798afa9a83bf71993e2
soonho <shyoon991@gmail.com>
2023-09-03 16:03:48 +0900

    DispatcherServlet 구현

---
+++ part1/mvc-practice/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,23 @@
+package org.example.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("DispatcherServlet#service");
+    }
+
+}
