eac06ff433f74961497788d06c0f5a0ade93eaf6
soonho <shyoon991@gmail.com>
2023-09-10 18:03:52 +0900

    #24 - Spring Security 설정을 추가
    지금은 테스트 개발 단계이기 때문에 모든 요청을 허용하도록 설정

---
+++ part2/project-board/src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.fastcampus.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authz -> {
+                    authz.anyRequest().permitAll();
+                })
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
