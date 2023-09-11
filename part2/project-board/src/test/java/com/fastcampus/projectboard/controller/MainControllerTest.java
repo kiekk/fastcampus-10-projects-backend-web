503a540adb1bbab2cc7f338ededb7ceca8d43327
soonho <shyoon991@gmail.com>
2023-09-11 21:13:52 +0900

    #26 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현
    MainController 를 만들어서
    기본 페이지가 게시판 페이지가 되도록 함
    강의 중에서는 redirect 로 접근했는데
    단순 조회 접근이고,
    url 루트 경로를 유지한 채로 게시판 페이지를
    대표로 만들고 싶은 상황이므로 forward 가 더 적절해보임

---
+++ part2/project-board/src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
@@ -0,0 +1,39 @@
+package com.fastcampus.projectboard.controller;
+
+import com.fastcampus.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 메인 페이지")
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("루트 경로('/')로 요청하면 게시판 페이지로 이동한다.")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(print());
+    }
+
+}
