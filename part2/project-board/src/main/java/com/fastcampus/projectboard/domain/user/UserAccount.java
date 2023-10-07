74035104530572855448fe81a2d401a9e43809a6
soonho <shyoon991@gmail.com>
2023-10-07 20:56:58 +0900

    #74 - 회원 엔티티 디자인 수정: 인증이 없는 상태에서 회원 정보를 DB에 등록하기 위한 생성자 메소드 추가
    인증 정보가 없을 땐 직접 생성자, 수정자 정보를 입력하여
    회원 등록을 할 수 있도록 생성자 메소드를 추가함

--- part2/project-board/src/main/java/com/fastcampus/projectboard/domain/user/UserAccount.java
+++ part2/project-board/src/main/java/com/fastcampus/projectboard/domain/user/UserAccount.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "userId"),
         @Index(columnList = "email", unique = true),
@@ -35,20 +35,25 @@ public class UserAccount extends AuditingFields {
     @Setter
     private String memo;
 
-
     protected UserAccount() {
     }
 
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
     }
 
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
