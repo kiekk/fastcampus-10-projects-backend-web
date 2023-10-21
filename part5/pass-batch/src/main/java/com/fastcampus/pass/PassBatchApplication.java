c495d27c99fee327ab6e5cfdd8dbb4edb6d30192
soonho <shyoon991@gmail.com>
2023-10-21 19:31:48 +0900

    간단하게 Job, Step 구성해보기
    * Spring Batch 5.x 부터는 설정 방법이 바뀌었기 때문에 아래 링크를 참고해서 작성.
    * https://velog.io/@calaf/Spring-Batch-%EC%82%AC%EC%9A%A9%ED%95%B4%EB%B3%B4%EA%B8%B0-1.-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0

--- part5/pass-batch/src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ part5/pass-batch/src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -1,13 +1,46 @@
 package com.fastcampus.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
 public class PassBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PassBatchApplication.class, args);
+    }
+
+    @Bean
+    public Job passJob(JobRepository jobRepository, Step passStep) {
+        return new JobBuilder("passStep", jobRepository)
+                .start(passStep)
+                .build();
+    }
+
+    @Bean
+    public Step passStep(final JobRepository jobRepository,
+                         final Tasklet passTasklet,
+                         final PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("passStep", jobRepository)
+                .tasklet(passTasklet, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Tasklet passTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("Execute PassStep");
+            return RepeatStatus.FINISHED;
+        });
     }
 
 }
