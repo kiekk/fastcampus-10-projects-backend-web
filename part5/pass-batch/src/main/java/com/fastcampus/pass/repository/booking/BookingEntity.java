1f89514720464bb59d7ce6b37995903ac42db3ad
soonho <shyoon991@gmail.com>
2023-10-23 21:06:57 +0900

    feat: 예약 관련 Entity & Repository 추가

---
+++ part5/pass-batch/src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
@@ -0,0 +1,48 @@
+package com.fastcampus.pass.repository.booking;
+
+import com.fastcampus.pass.repository.BaseEntity;
+import com.fastcampus.pass.repository.pass.PassEntity;
+import com.fastcampus.pass.repository.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "booking")
+public class BookingEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingSeq;
+    private Long passSeq;
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+    private boolean usedPass;
+    private boolean attended;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private LocalDateTime cancelledAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private PassEntity passEntity;
+
+    // endedAt 기준, yyyy-MM-HH 00:00:00
+    public LocalDateTime getStatisticsAt() {
+        return this.endedAt.withHour(0).withMinute(0).withSecond(0).withNano(0);
+
+    }
+
+}
---
+++ part5/pass-batch/src/main/java/com/fastcampus/pass/repository/booking/BookingRepository.java
@@ -0,0 +1,18 @@
+package com.fastcampus.pass.repository.booking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
+    @Transactional
+    @Modifying
+    @Query(value = """
+            UPDATE BookingEntity b
+            SET b.usedPass = :usedPass
+            , b.modifiedAt = CURRENT_TIMESTAMP
+            WHERE b.passSeq = :passSeq
+            """)
+    int updateUsedPass(Integer passSeq, boolean usedPass);
+}
