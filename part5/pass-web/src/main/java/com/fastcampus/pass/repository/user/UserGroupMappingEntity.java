b0bc623c9da7fa47e5b59076935f8535bb8f745a
soonho <shyoon991@gmail.com>
2023-10-24 19:42:42 +0900

    feat: Entity & Repository & Util 추가

---
+++ part5/pass-web/src/main/java/com/fastcampus/pass/repository/user/UserGroupMappingEntity.java
@@ -0,0 +1,27 @@
+package com.fastcampus.pass.repository.user;
+
+import com.fastcampus.pass.repository.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_group_mapping")
+@IdClass(UserGroupMappingId.class)
+public class UserGroupMappingEntity extends BaseEntity {
+    @Id
+    private String userGroupId;
+    @Id
+    private String userId;
+
+    private String userGroupName;
+    private String description;
+
+}
