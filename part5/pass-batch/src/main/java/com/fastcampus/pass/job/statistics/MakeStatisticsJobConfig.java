b2c19f62222d95c13b65ee75b5e885167326e2a6
soonho <shyoon991@gmail.com>
2023-10-24 18:50:21 +0900

    feat: 통계 데이터 생성 잡 추가

---
+++ part5/pass-batch/src/main/java/com/fastcampus/pass/job/statistics/MakeStatisticsJobConfig.java
@@ -0,0 +1,143 @@
+package com.fastcampus.pass.job.statistics;
+
+import com.fastcampus.pass.repository.booking.BookingEntity;
+import com.fastcampus.pass.repository.statistics.StatisticsEntity;
+import com.fastcampus.pass.repository.statistics.StatisticsRepository;
+import com.fastcampus.pass.util.LocalDateTimeUtils;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class MakeStatisticsJobConfig {
+    private final int CHUNK_SIZE = 10;
+
+    private final EntityManagerFactory entityManagerFactory;
+    private final StatisticsRepository statisticsRepository;
+    private final MakeDailyStatisticsTasklet makeDailyStatisticsTasklet;
+    private final MakeWeeklyStatisticsTasklet makeWeeklyStatisticsTasklet;
+
+    @Bean
+    Job makeStatisticsJob(final JobRepository jobRepository,
+                          final Step addStatisticsStep,
+                          final Step makeDailyStatisticsStep,
+                          final Step makeWeeklyStatisticsStep) {
+        Flow addStatisticsFlow = new FlowBuilder<Flow>("addStatisticsFlow")
+                .start(addStatisticsStep)
+                .build();
+
+        Flow makeDailyStatisticsFlow = new FlowBuilder<Flow>("makeDailyStatisticsFlow")
+                .start(makeDailyStatisticsStep)
+                .build();
+
+        Flow makeWeeklyStatisticsFlow = new FlowBuilder<Flow>("makeWeeklyStatisticsFlow")
+                .start(makeWeeklyStatisticsStep)
+                .build();
+
+        Flow parallelMakeStatisticsFlow = new FlowBuilder<Flow>("parallelMakeStatisticsFlow")
+                .split(new SimpleAsyncTaskExecutor())
+                .add(makeDailyStatisticsFlow, makeWeeklyStatisticsFlow)
+                .build();
+
+        return new JobBuilder("makeStatisticsJob", jobRepository)
+                .start(addStatisticsFlow)
+                .next(parallelMakeStatisticsFlow)
+                .build()
+                .build();
+    }
+
+    @Bean
+    public Step addStatisticsStep(final JobRepository jobRepository,
+                                  final PlatformTransactionManager platformTransactionManager,
+                                  final ItemReader<BookingEntity> addStatisticsItemReader,
+                                  final ItemWriter<BookingEntity> addStatisticsItemWriter) {
+        return new StepBuilder("addStatisticsStep", jobRepository)
+                .<BookingEntity, BookingEntity>chunk(CHUNK_SIZE, platformTransactionManager)
+                .reader(addStatisticsItemReader)
+                .writer(addStatisticsItemWriter)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaCursorItemReader<BookingEntity> addStatisticsItemReader(@Value("#{jobParameters[from]}") String fromString,
+                                                                      @Value("#{jobParameters[to]}") String toString) {
+        final LocalDateTime from = LocalDateTimeUtils.parse(fromString);
+        final LocalDateTime to = LocalDateTimeUtils.parse(toString);
+
+        return new JpaCursorItemReaderBuilder<BookingEntity>()
+                .name("usePassesItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                // JobParameter를 받아 종료 일시(endedAt) 기준으로 통계 대상 예약(Booking)을 조회합니다.
+                .queryString("select b from BookingEntity b where b.endedAt between :from and :to")
+                .parameterValues(Map.of("from", from, "to", to))
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<BookingEntity> addStatisticsItemWriter() {
+        return bookingEntities -> {
+            Map<LocalDateTime, StatisticsEntity> statisticsEntityMap = new LinkedHashMap<>();
+
+            for (BookingEntity bookingEntity : bookingEntities) {
+                final LocalDateTime statisticsAt = bookingEntity.getStatisticsAt();
+                StatisticsEntity statisticsEntity = statisticsEntityMap.get(statisticsAt);
+
+                if (statisticsEntity == null) {
+                    statisticsEntityMap.put(statisticsAt, StatisticsEntity.create(bookingEntity));
+
+                } else {
+                    statisticsEntity.add(bookingEntity);
+
+                }
+
+            }
+            final List<StatisticsEntity> statisticsEntities = new ArrayList<>(statisticsEntityMap.values());
+            statisticsRepository.saveAll(statisticsEntities);
+            log.info("### addStatisticsStep 종료");
+
+        };
+    }
+
+    @Bean
+    public Step makeDailyStatisticsStep(final JobRepository jobRepository,
+                                        final PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("makeDailyStatisticsStep", jobRepository)
+                .tasklet(makeDailyStatisticsTasklet, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step makeWeeklyStatisticsStep(final JobRepository jobRepository,
+                                         final PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("makeWeeklyStatisticsStep", jobRepository)
+                .tasklet(makeWeeklyStatisticsTasklet, platformTransactionManager)
+                .build();
+    }
+
+}
